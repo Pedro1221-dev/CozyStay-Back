1f158a4e044f782edd84f1479f4b137afb1af020
Nerf22117 <40220191@esmad.ipp.pt>
2024-05-07 16:17:07 +0100

    fixed pagination, now pagination reflects query search

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,19 +17,12 @@ exports.findAll = async (req, res) => {
         // Initialize search options object
         const searchOptions = {};
 
-        // Retrieve all users from the database
-        let users = await User.findAll();
-
         // Destructure query parameters
         const { limit: queryLimit, page: queryPage, name, blocked} = req.query;
 
         // Pagination information
-        // Calculate the total number of users
-        const totalUsers = users.length;
         // Set the limit per page, defaulting to 10 if not specified in the query parameters
         const limit = queryLimit ? parseInt(queryLimit) : 10;
-        // Calculate the total number of pages based on the total number of users and the limit per page
-        const totalPages = Math.ceil(totalUsers / limit);
         // Determine the current page, defaulting to 1 if not specified in the query parameters
         const currentPage = queryPage ? parseInt(queryPage) : 1;
         // Calculate the offset for pagination, which determines the starting index of the users to be fetched for the current page
@@ -52,12 +45,7 @@ exports.findAll = async (req, res) => {
         if (blocked && !(blocked === '0' || blocked === '1')) {
             return res.status(400).json({ message: "O parâmetro 'blocked' deve ser um valor booleano ('0' for false and '1' for true)"});
         }
-
-        // Handle out-of-range page number
-        if (currentPage > totalPages) {
-            return res.status(404).json({ message: "No more results available" });
-        }
-
+        
         // Validate query parameters
         if (isNaN(limit) || limit <= 5) {
             return res.status(400).json({ message: "Limit must be a positive integer, greater than 5" });
@@ -67,6 +55,19 @@ exports.findAll = async (req, res) => {
             return res.status(400).json({ message: "Page must be 1 or a positive integer" });
         }
 
+        // Find users with pagination and search options
+        const users = await User.findAll({
+            limit: limit,
+            offset: offset,
+            ...searchOptions,
+        });
+
+        // Calculate the total number of users after applying pagination
+        const totalUsers = await User.count({
+            where: searchOptions.where // Apply the same search options for counting
+        });
+        // Calculate the total number of pages based on the total number of properties and the limit per page
+        const totalPages = Math.ceil(totalUsers / limit);
         // Previous and next page links
         // Calculate the page number for the previous page
         const previousPage = currentPage > 1 ? currentPage - 1 : null;
@@ -76,12 +77,10 @@ exports.findAll = async (req, res) => {
         // Otherwise, set nextPage to null
         const nextPage = currentPage < totalPages ? currentPage + 1 : (currentPage === 1 ? 2 : null);
 
-        // Find users with pagination and search options
-        users = await User.findAll({
-            limit: limit,
-            offset: offset,
-            ...searchOptions,
-        });
+        // Handle out-of-range page number
+        if (currentPage > totalPages) {
+            return res.status(404).json({ message: "No more results available" });
+        }
 
         // Handle no results found
         if (users.length === 0) {
@@ -96,37 +95,46 @@ exports.findAll = async (req, res) => {
                 { "rel": "delete", "href": `/users/${user.user_id}`, "method": "DELETE" },
                 { "rel": "modify", "href": `/users/${user.user_id}`, "method": "PATCH" }
             ]
-      }));
+        }));
       
         // Send response with pagination and data
-      res.status(200).json({ 
-        success: true, 
-        pagination: {
-            total: totalUsers,
-            pages: totalPages,
-            current: currentPage,
-            limit: limit
-        },
-        data: usersWithLinks,
-        links: [
-          { 
-            "rel": "add-user", 
-            "href": `/users`, 
-            "method": "POST" 
-          },
-          { 
-            "rel": "previous-page",
-            "href": `/users?limit=${limit}&page=${previousPage}`,
-            "method": "GET"
-          },
-          { 
-            "rel": "next-page",
-            "href": `/users?limit=${limit}&page=${nextPage}`,
-            "method": "GET"
-          }
-        ]
-      });
-        
+        res.status(200).json({ 
+            success: true, 
+            pagination: {
+                total: totalUsers,
+                pages: totalPages,
+                current: currentPage,
+                limit: limit
+            },
+            data: usersWithLinks,
+            links: [
+            { 
+                "rel": "add-user", 
+                "href": `/users`, 
+                "method": "POST" 
+            },
+            { 
+                "rel": "previous-page",
+                "href": `/users?limit=${limit}&page=${previousPage}`,
+                "method": "GET"
+            },
+            { 
+                "rel": "next-page",
+                "href": `/users?limit=${limit}&page=${nextPage}`,
+                "method": "GET"
+            },
+            { 
+                "rel": "last-page",
+                "href": `/users?limit=${limit}&page=${totalPages}`,
+                "method": "GET"
+            },
+            { 
+                "rel": "first-page",
+                "href": `/users?limit=${limit}&page=1`,
+                "method": "GET"
+            }
+            ]
+        });
     } catch (error) {
         // If an error occurs, respond with an error status code and message
         res.status(500).json({ 
