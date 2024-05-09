a3a58a0de733436386d4a318a77b919c3e80d6cb
Nerf22117 <40220191@esmad.ipp.pt>
2024-05-09 12:41:26 +0100

    Imported middlewares, added comments, added route to login

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,43 +1,49 @@
+// Importing the express module
 const express = require('express');
+// Creating an instance of the express router
 const router = express.Router();
+// Importing middleware functions
+const checkAuth = require('../middleware/check-auth');
+const checkCurrent = require('../middleware/check-current');
+const checkAdmin= require('../middleware/check-admin');
 
 // import user controller middleware
 const userController = require("../controllers/user.controller");
 
 router.route('/')
-    .get( userController.findAll )
-    .post(userController.create) 
+    .get( userController.findAll ) // PROTECTED
+    .post(userController.create)  // PUBLIC
 
 router.route('/:user_id')
-    .get( userController.findOne ) 
-    .delete( userController.delete)
-    .patch(userController.update)
+    .get( userController.findOne )  // PUBLIC
+    .delete( checkAuth, userController.delete) // PROTECTED
+    .patch( checkAuth, checkCurrent, userController.update) // PROTECTED
 
 router.route('/:user_id/properties')
-    //.get( usersController.xxx ) 
+    //.get( usersController.xxx )  // PUBLIC
 
 router.route('/current')
-    //.get( usersController.xxx )
+    //.get( usersController.xxx )  // PROTECTED
     //.patch( usersController.xxx )
 
 router.route('/current/properties')
-    //.get( usersController.xxx )
+    //.get( usersController.xxx ) // PROTECTED
 
 router.route('/current/bookings')
-    //.get( usersController.xxx )
+    //.get( usersController.xxx ) // PROTECTED
 
 router.route('/current/favorites')
-    //.get( usersController.xxx )
-    //.post( usersController.xxx )
+    //.get( usersController.xxx ) // PROTECTED
+    //.post( usersController.xxx ) // PROTECTED
 
 router.route('/current/favorites/:property_id')
-    //.delete( usersController.xxx )
+    //.delete( usersController.xxx ) // PROTECTED
 
 router.route('/verifyEmail')
-    //.post( usersController.xxx )
+    //.post( usersController.xxx ) // PUBLIC
 
 router.route('/login')
-    .post( userController.login )
+    .post( userController.login ) // PUBLIC
 
 
 router.all('*', (req, res) => {
