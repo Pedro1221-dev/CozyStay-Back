f3e7db3b83fc3b4d35cd5ac4df968a853aa9549f
Simão Freitas <40220191@esmad.ipp.pt>
2024-05-27 12:22:42 +0100

    Updated route to register a property

--- routes/properties.routes.js
+++ routes/properties.routes.js
@@ -2,13 +2,15 @@
 const express = require('express');
 // Creating an instance of the express router
 const router = express.Router();
+// Importing middleware functions
+const checkAuth = require('../middleware/check-auth');
 
 // import property controller middleware
 const propertyController = require("../controllers/property.controller");
 
 router.route('/')
     .get( propertyController.findAll ) // PUBLIC
-    .post ( propertyController.create ) // PROTECTED
+    .post ( checkAuth, propertyController.create ) // PROTECTED (user logged in)
 
 router.route('/:property_id')
     .get( propertyController.findOne )  // PUBLIC
