ebd99cde5c4bebcf8bc139d597c4a6d93d9093c6
Nerf22117 <40220191@esmad.ipp.pt>
2024-05-29 19:23:44 +0100

    Added comments

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,10 +1,13 @@
+// Import the cloudinary library
 const cloudinary = require("cloudinary").v2;
 
+// Configure Cloudinary with the credentials from environment variables
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+// Export the configured Cloudinary instance
 module.exports = cloudinary;
 
