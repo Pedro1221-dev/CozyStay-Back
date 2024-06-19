0b8d49f0819708897307c05fa1590f068468e82c
Simão Freitas <40220191@esmad.ipp.pt>
2024-06-19 17:29:18 +0100

    Minor fix

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -257,7 +257,7 @@ exports.create = async (req, res) => {
         res.status(201).json({
             success: true,
             msg: "Booking successfully created.",
-            data: `Booking id: ${newBooking.booking_id}`,
+            booking_id: newBooking.booking_id,
             links: [
                 { "rel": "self", "href": `/bookings/${newBooking.booking_id}`, "method": "GET" },
                 { "rel": "delete", "href": `/bookings/${newBooking.booking_id}`, "method": "DELETE" },
